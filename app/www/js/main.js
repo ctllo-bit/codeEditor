cf0c42561d3e3ffe3ddab37c4cf65902b7c0aa73
CTLLO <10153479+caoctllo@user.noreply.gitee.com>
2025-12-29 08:22:04 +0800

    fixed one

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -1,9 +1,25 @@
-const iframe = window.frameElement;
-iframe.sandbox ="allow-same-origin allow-scripts allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-downloads";
-//iframe.src = "https://g2.igroutka.ru/games/164/OUNWHVSFldaeghn4/1/subway_surfers_hong_kong/?winid=id-fc5a6e6796ce2dcff1e83842a3a014fc";
-iframe.src = "https://g2.igroutka.ru/games/6/murder/6qidqpxlp5b2ewkjwh3a8ggt0sg0cepi/?_=1766848240810&amp;appTo";
-console.log('✅ 外部JS文件加载成功！');
+// 尝试获取当前父级 iframe 元素
+let iframe = window.frameElement;
+let targetURL='https://g2.igroutka.ru/games/6/murder/6qidqpxlp5b2ewkjwh3a8ggt0sg0cepi/';
+
+if(iframe){
+    iframe.sandbox ="allow-same-origin allow-scripts allow-forms allow-modals allow-popups allow-popups-to-escape-sandbox allow-downloads";
+    iframe.src = targetURL;
+    console.log('✅ 修改iframe成功！');
+}else{
+//飞牛APP因跨源（cross-origin)获取不了，则直接跳转
+//window.location.href = targetURL;
+window.open(targetURL, '_top');
 window.alert(5 + 6);
+}
+
+
+
+
+
+
+
+
 
 
 
